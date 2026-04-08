3e345591e7c82c7e057e002ee885cffbbf80c3b4
NBZion <subtopewds0921@gmail.com>
2026-04-08 18:43:55 +0800

    Initial Commit

---
+++ client.py
@@ -0,0 +1,83 @@
+import os
+import socket
+import sys
+import threading
+
+client_socket = None
+connected = False
+CLIENT_STORAGE_DIR = "ClientStorage/"
+
+
+def receive_messages():
+    global connected
+    while connected:
+        try:
+            message = client_socket.recv(1024).decode()
+            if not message:
+                print("\n[SERVER DISCONNECTED]")
+                connected = False
+                break
+
+            print(f"{message}")
+        except Exception as e:
+            if connected:
+                print(f"\n[Error] Connection lost: {e}")
+            connected = False
+            break
+
+
+def start_client():
+    global client_socket, connected
+
+    while True:
+        try:
+            user_input = input(">> ").strip()
+            if not user_input:
+                continue
+
+            input_split = user_input.split(" ")
+            command = input_split[0].lower()
+
+            match command:
+                case "/?":
+                    print("\n--- Command Help ---")
+                    print("/join <server_ip_add> <port> : Connect to server")
+                    print("/leave                       : Disconnect from server")
+                    print("/register <handle>           : Register alias")
+                    print("/store <filename>            : Send file to server")
+                    print("/dir                         : Request directory list")
+                    print("/get <filename>              : Fetch file from server")
+                    print("--------------------\n")
+                    continue
+                case "/join":
+                    if connected:
+                        print("Error: Already connected to server")
+                        continue
+                    if len(input_split) != 3:
+                        print("Error: Syntax is join <server_ip_add> <port")
+                        continue
+
+                    ip, port = input_split[1], int(input_split[2])
+
+                    try:
+                        client_socket = socket.socket(
+                            socket.AF_INET, socket.SOCK_STREAM
+                        )
+                        client_socket.connect((ip, port))
+                        connected = True
+
+                        receive_thread = threading.Thread(target=receive_messages)
+                        receive_thread.daemon = True
+                        receive_thread.start()
+                    except Exception as e:
+                        print(f"Error connecting to server: {e}")
+        except KeyboardInterrupt:
+            if connected:
+                client_socket.send("disconnect".encode())
+                client_socket.close()
+            print("\nExiting Client")
+            sys.exit()
+
+
+if __name__ == "__main__":
+    start_client()
---
+++ server.py
@@ -0,0 +1,63 @@
+import datetime
+import os
+import socket
+import sys
+import threading
+
+HOST = sys.argv[1]
+PORT = int(sys.argv[2])
+SERVER_STORAGE_DIR = "ServerStorage/"
+
+connected_clients = {}
+
+
+def handle_client(conn, addr):
+    print(f"[NEW Connection] {addr} connected.")
+
+    conn.send("connect success".encode())
+
+    connected_clients[conn] = {"handle": None}
+
+    connected = True
+    while connected:
+        try:
+            message = conn.recv(1024).decode().strip()
+            if not message:
+                break
+
+            mSplit = message.split(" ")
+            command = mSplit[1]
+            args = mSplit[1] if len(mSplit) > 1 else ""
+
+            match command:
+                case "disconnect":
+                    conn.send("disconnect success".encode())
+                    connected = False
+
+        except Exception as e:
+            print(f"[ERROR] {addr}: {e}")
+            break
+
+    if conn in connected_clients:
+        del connected_clients[conn]
+
+    conn.close()
+    print(f"f[DISCONNECTED] {addr} disconnected")
+
+
+def start_server():
+    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server.bind((HOST, PORT))
+    server.listen()
+
+    while True:
+        # Wait for Clients
+        conn, addr = server.accept()
+
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f"[ACTIVE CONNECTIONS] {threading.active_count() - 1}")
+
+
+if __name__ == "__main__":
+    start_server()
